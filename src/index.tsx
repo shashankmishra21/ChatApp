46dfa1d62f9cf3059d35d227d214b734a3726576
Shashank Mishra <mishrashashank2106@gmail.com>
2025-08-05 04:01:45 +0530

    project initialized and one way comm (from client to server done in console log)

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { WebSocketServer} from 'ws';
+
+const wss = new WebSocketServer({ port : 8080}) ;
+
+let userCount = 0;
+
+wss.on("connection" , (socket) =>{
+    userCount = userCount +1;
+    console.log("userconencted #" + userCount);
+    
+    socket.on("message" , (message) =>{
+        console.log("message received " + message.toString())
+    } )
+})

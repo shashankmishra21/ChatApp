3536ffc5386931a75ba07c876588ceaca6d2384d
Shashank Mishra <mishrashashank2106@gmail.com>
2025-08-05 05:12:58 +0530

    messages send from client to all the client connected with the server

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,17 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
 let userCount = 0;
+let allSockets = [];
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
     userCount = userCount + 1;
     console.log("userconencted #" + userCount);
     socket.on("message", (message) => {
-        console.log("message received" + message.toString());
+        console.log("message received " + message.toString());
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(message.toString() + " : semt from the server");
+        }
     });
 });
 //# sourceMappingURL=index.js.map
